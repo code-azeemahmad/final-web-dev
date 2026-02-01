e1c9465e779259c8e5f54814b31a239b9096f74e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-02-01 07:20:34 +0500

    Introduction to JS

---
+++ JS/introduction/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("hello Azeem");
+
+document.title = "hey! I am good";
+
+document.body.style.backgroundColor = "red";        // adds inline css
+
+let a = prompt("Enter your number: ");
+
+console.log("Your Number is " + a);
+
+let isTrue = confirm("Are you sure to sacrifice everything?");
+
+if (isTrue)
+{
+    console.log("be ready to face");
+}
+else 
+{
+    console.log("No need");
+}
