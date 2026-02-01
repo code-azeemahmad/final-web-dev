d5c4805b22c55653e49558d842481c10ca35af86
code-azeemahmad <azeemahmadd9@gmail.com>
2026-02-01 20:20:13 +0500

    Walking through the DOM

---
+++ JS/DOM2/script.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!");
+
+// document.body.childNodes;
+// document.body.childNodes[0];
+
+// new line is also considered text node(not an element)
+// comment is a comment node
+// before and after comment node is a text node
+
+// let cont = document.body.childNodes[1]
+
+// cont.firstChild
+// cont.firstElementChild
+// cont.lastChild
+// cont.lastElementChild
+
+// cont.firstElementChild.style.backgroundColor = "red";
+
+// cont.children        // give all element child nodes
+
+
+// cont.children[1]
+// cont.children[1].nextElementSibling
+// cont.children[1].previousElementSibling
+// cont.children[1].previousSibling
